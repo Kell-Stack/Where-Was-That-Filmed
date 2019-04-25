bc608ba61bd9ea6150ae789a730a489e90d02d1f
Kelly Sousa <45135493+Kell-Stack@users.noreply.github.com>
2019-04-24 19:10:38 -0700

    add component files, grabbed json data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
-// import Header from './Header.js'
+import Header from './Header.js'
+import AllTitles from './AllTitles.js'
 // import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+    <Header
+
+    />
+
+
+
+    </div>
 
   );
 }
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
-// class Header extends React.Component {
-// 	render(){
-// 		return (
-// 			<div className="header">
-//                 class HomePage extends React.Component {
-// 	render(){
-// 		const {photos} = data
-// 		console.log(photos);
-// 		return (
-
-// }
-// 			</div>
-// 		)
-// 	}
-// }
+class Header extends React.Component {
+	render(){
+		return (
+			<div className="header">
+				<h3>Where Was That Filmed?</h3>
+			</div>
+		)
+	}
+}
 
 export default Header;
+
+//styling plan => put logo on top left
